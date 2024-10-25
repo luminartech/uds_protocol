99b08f6edd0fefc6437142de42ab7a98ed7c2d20
Zach <zheylmun@gmail.com>
2024-10-25 15:00:45 -0400

    Remove old UDSRequest type

--- src/request.rs
+++ src/request.rs
@@ -1,134 +1,15 @@
+//! Module for making and handling UDS Requests
+
 use std::io::{Read, Write};
 
-use byteorder::{ReadBytesExt, WriteBytesExt};
-use serde::{Deserialize, Serialize};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
 use crate::Error;
 
 use super::{
     service::UdsServiceType, CommunicationEnable, CommunicationType, DtcSettings, EcuResetType,
-    CommunicationEnable, CommunicationType, DtcSettings, EcuResetType, RoutineControlSubFunction,
-    SessionType, SUCCESS,
+    RoutineControlSubFunction, SessionType, SUCCESS,
 };
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct UdsRequest {
-    data: Vec<u8>,
-}
-
-impl UdsRequest {
-    pub fn from_service_type(service_type: UdsRequestType) -> Self {
-        match service_type {
-            UdsRequestType::CommunicationControl(communication_control) => {
-                let suppression_byte = match communication_control.suppress_response {
-                    true => POSITIVE_RESPONSE,
-                    false => 0x00,
-                };
-                let enable_byte: u8 = communication_control.communication_enable.into();
-                Self {
-                    data: vec![
-                        communication_control
-                            .get_service_type()
-                            .request_service_to_byte(),
-                        enable_byte | suppression_byte,
-                        communication_control.communication_type.into(),
-                    ],
-                }
-            }
-            UdsRequestType::ControlDTCSettings(dtc_settings) => {
-                let suppression_byte = match dtc_settings.suppress_response {
-                    true => POSITIVE_RESPONSE,
-                    false => 0x00,
-                };
-                let dtc_setting_byte: u8 = dtc_settings.setting.into();
-                Self {
-                    data: vec![
-                        dtc_settings.get_service_type().request_service_to_byte(),
-                        dtc_setting_byte | suppression_byte,
-                    ],
-                }
-            }
-            UdsRequestType::DiagnosticSessionControl(session_control) => Self {
-                data: vec![
-                    session_control.get_service_type().request_service_to_byte(),
-                    session_control.session_type.into(),
-                ],
-            },
-            UdsRequestType::EcuReset(ecu_reset) => Self {
-                data: vec![
-                    ecu_reset.get_service_type().request_service_to_byte(),
-                    ecu_reset.reset_type.into(),
-                ],
-            },
-            UdsRequestType::ReadDataByIdentifier(read_data_by_identifier) => {
-                let did_bytes = read_data_by_identifier.did.to_be_bytes();
-                Self {
-                    data: vec![
-                        read_data_by_identifier
-                            .get_service_type()
-                            .request_service_to_byte(),
-                        did_bytes[0],
-                        did_bytes[1],
-                    ],
-                }
-            }
-            UdsRequestType::RequestDownload(request_download) => {
-                let mut data = vec![
-                    request_download
-                        .get_service_type()
-                        .request_service_to_byte(),
-                    request_download.data_format_identifier,
-                    request_download.address_and_length_format_identifier,
-                ];
-                data.extend_from_slice(request_download.memory_address.to_be_bytes().as_slice());
-                data.extend_from_slice(request_download.memory_size.to_be_bytes().as_slice());
-                Self { data }
-            }
-            UdsRequestType::RequestTransferExit(_) => Self {
-                data: vec![UdsServiceType::RequestTransferExit.request_service_to_byte()],
-            },
-            UdsRequestType::RoutineControl(routine_control) => {
-                let mut data = vec![routine_control.get_service_type().request_service_to_byte()];
-                data.push(routine_control.sub_function.into());
-                data.extend_from_slice(&routine_control.routine_id.to_be_bytes());
-                data.extend_from_slice(routine_control.data.as_slice());
-                Self { data }
-            }
-            UdsRequestType::TesterPresent(tester_present) => Self {
-                data: (vec![
-                    tester_present.get_service_type().request_service_to_byte(),
-                    SUCCESS,
-                ]),
-            },
-            UdsRequestType::TransferData(transfer_data) => {
-                let mut data = vec![transfer_data.get_service_type().request_service_to_byte()];
-                data.push(transfer_data.sequence);
-                data.extend_from_slice(transfer_data.data.as_slice());
-                Self { data }
-            }
-            UdsRequestType::WriteDataByIdentifier(write_data_by_identifier) => {
-                let mut data = vec![write_data_by_identifier
-                    .get_service_type()
-                    .request_service_to_byte()];
-                data.extend_from_slice(&write_data_by_identifier.did.to_be_bytes());
-                data.extend_from_slice(write_data_by_identifier.data.as_slice());
-                Self { data }
-            }
-        }
-    }
-
-    pub fn service_type(&self) -> UdsServiceType {
-        UdsServiceType::service_from_request_byte(self.data[0])
-    }
-
-    pub fn to_network(&self) -> Vec<u8> {
-        self.data.clone()
-    }
-
-    pub fn expected_response_byte(&self) -> u8 {
-        self.service_type().response_to_byte()
-    }
-}
 
 pub struct CommunicationControl {
     pub communication_enable: CommunicationEnable,
