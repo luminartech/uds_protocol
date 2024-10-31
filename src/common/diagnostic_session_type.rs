1aaec85eccd89698f72245f15726386933adef70
Zach <zheylmun@gmail.com>
2024-10-31 17:03:45 -0400

    Start fleshing out `DiagnosticSessionType` per the spec

--- src/common/diagnostic_session_type.rs
+++ src/common/diagnostic_session_type.rs
@@ -1,21 +1,45 @@
 use clap::ValueEnum;
+
 use serde::{Deserialize, Serialize};
 
+/// `DiagnosticSessionType` is used to specify or describe the session type of the server
 #[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize, ValueEnum)]
 pub enum DiagnosticSessionType {
-    Default,
-    Programming,
-    Extended,
-    Safety,
+    /// This value is reserved by the ISO 14229-1 Specification
+    #[clap(skip)]
+    ISOSAEReserved(u8),
+    /// The `DefaultSession` enables the standard diagnostic functionality
+    /// - No TesterPresent messages are required to remain in this session
+    /// - Any other diagnostic sessions are stopped upon succesful entry into this session
+    /// - Any security authorization is revoked
+    /// - This session is initialized on startup
+    DefaultSession,
+    /// The `ProgrammingSession` enables services required to support writing server memory
+    /// - Upon timeout the server shall return to the `DefaultSession`
+    /// - Success response may be sent before or after session is actually entered
+    ProgrammingSession,
+    /// The `ExtendedDiagnosticSession` enables additional diagnostics functionality which can modify server behavior
+    ExtendedDiagnosticSession,
+    /// The `SafetySystemDiagnosticSession` enables diagnostics functionality for safety systems
+    SafetySystemDiagnosticSession,
+    /// Value reserved for use by vehicle manufacturers
+    #[clap(skip)]
+    VehicleManufacturerSpecificSession(u8),
+    /// Value reserved for use by system suppliers
+    #[clap(skip)]
+    SystemSupplierSpecificSession(u8),
 }
 
 impl From<DiagnosticSessionType> for u8 {
     fn from(value: DiagnosticSessionType) -> Self {
         match value {
-            DiagnosticSessionType::Default => 0x01,
-            DiagnosticSessionType::Programming => 0x02,
-            DiagnosticSessionType::Extended => 0x03,
-            DiagnosticSessionType::Safety => 0x04,
+            DiagnosticSessionType::ISOSAEReserved(value) => value,
+            DiagnosticSessionType::DefaultSession => 0x01,
+            DiagnosticSessionType::ProgrammingSession => 0x02,
+            DiagnosticSessionType::ExtendedDiagnosticSession => 0x03,
+            DiagnosticSessionType::SafetySystemDiagnosticSession => 0x04,
+            DiagnosticSessionType::VehicleManufacturerSpecificSession(value) => value,
+            DiagnosticSessionType::SystemSupplierSpecificSession(value) => value,
         }
     }
 }
@@ -23,11 +47,16 @@ impl From<DiagnosticSessionType> for u8 {
 impl From<u8> for DiagnosticSessionType {
     fn from(value: u8) -> Self {
         match value {
-            0x01 => DiagnosticSessionType::Default,
-            0x02 => DiagnosticSessionType::Programming,
-            0x03 => DiagnosticSessionType::Extended,
-            0x04 => DiagnosticSessionType::Safety,
-            _ => panic!("Invalid session type: {}", value),
+            0x00 => DiagnosticSessionType::ISOSAEReserved(value),
+            0x01 => DiagnosticSessionType::DefaultSession,
+            0x02 => DiagnosticSessionType::ProgrammingSession,
+            0x03 => DiagnosticSessionType::ExtendedDiagnosticSession,
+            0x04 => DiagnosticSessionType::SafetySystemDiagnosticSession,
+            0x05..=0x3F => DiagnosticSessionType::ISOSAEReserved(value),
+            0x40..=0x5F => DiagnosticSessionType::VehicleManufacturerSpecificSession(value),
+            0x60..=0x7E => DiagnosticSessionType::SystemSupplierSpecificSession(value),
+            0x7F => DiagnosticSessionType::ISOSAEReserved(value),
+            _ => unreachable!("The suppress positive response bit has been masked off, this should be unreachable")
         }
     }
 }
