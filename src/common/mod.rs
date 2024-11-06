da33f00ebbd5a019720744a33bcd5751c137bf41
Zach <zheylmun@gmail.com>
2024-11-06 13:22:37 -0500

    Add NRC enum for all negative responses

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,5 +1,8 @@
 mod diagnostic_session_type;
 pub use diagnostic_session_type::DiagnosticSessionType;
 
+mod negative_response_code;
+pub use negative_response_code::NegativeResponseCode;
+
 mod suppressable_positive_response;
 pub use suppressable_positive_response::SuppressablePositiveResponse;
