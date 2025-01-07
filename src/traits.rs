d621d506f4130701a8eba3ef50ddaa69cc4f5200
Zach <zheylmun@gmail.com>
2025-01-07 17:53:27 -0500

    Add initial WireFormat trait to uds_protocol

---
+++ src/traits.rs
@@ -0,0 +1,9 @@
+/// A trait for types that can be serialized and deserialized from a byte stream.
+pub trait WireFormat<E>: Sized
+where
+    E: std::error::Error,
+{
+    fn from_reader<T: std::io::Read>(reader: &mut T) -> Result<Self, E>;
+
+    fn to_writer<T: std::io::Write>(&self, writer: &mut T) -> Result<usize, E>;
+}
