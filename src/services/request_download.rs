9d2d2023124ea8d8a4e8d9848f86c69e82ef5001
Kim <kim@squeakytrees.com>
2025-01-11 01:34:35 -0500

    Add RequestDownloadResponse

--- src/services/request_download.rs
+++ src/services/request_download.rs
@@ -6,6 +6,8 @@ use crate::{Error, SingleValueWireFormat, WireFormat};
 const MEMORY_SIZE_NIBBLE_MASK:    u8 = 0b1111_0000;
 const MEMORY_ADDRESS_NIBBLE_MASK: u8 = 0b0000_1111;
 
+const BLOCK_LENGTH_NIBBLE_MASK:   u8 = 0b1111_0000;
+
 /// Decoded from the `address_and_length_format_identifier` field of the [`RequestDownloadRequest`] struct
 /// 
 /// See ISO-14229-1:2020, Table H.1 for format information
@@ -101,3 +103,65 @@ impl WireFormat for RequestDownloadRequest {
 }
 
 impl SingleValueWireFormat for RequestDownloadRequest {}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct LengthFormatIdentifier {
+    max_number_of_block_length: u8,
+}
+
+impl From<u8> for LengthFormatIdentifier {
+    fn from(value: u8) -> Self {
+        Self {
+            max_number_of_block_length: (value & BLOCK_LENGTH_NIBBLE_MASK) >> 4,
+        }
+    }
+}
+impl From<LengthFormatIdentifier> for u8 {
+    fn from(length_format_identifier: LengthFormatIdentifier) -> u8 {
+        length_format_identifier.max_number_of_block_length << 4
+    }
+}
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[non_exhaustive]
+pub struct RequestDownloadResponse {
+    /// Format is similar to `address_and_length_format_identifier` field of the [`RequestDownloadRequest`] struct
+    /// As in it is a nibble with the high nibble being the length of the max_number_of_block_length field
+    pub length_format_identifier: LengthFormatIdentifier,
+    /// Variable length field, length determined by `length_format_identifier`
+    /// Client is instructed to send this many bytes per [`TransferDataRequest`] message.
+    pub max_number_of_block_length: Vec<u8>,
+}
+
+impl RequestDownloadResponse {
+    pub(crate) fn new(
+        length_format_identifier: u8,
+        max_number_of_block_length: Vec<u8>,
+    ) -> Self {
+        Self { 
+            length_format_identifier: LengthFormatIdentifier::from(length_format_identifier),
+            max_number_of_block_length
+        }
+    }
+}
+
+impl WireFormat for RequestDownloadResponse {
+    fn option_from_reader<T: std::io::Read>(reader: &mut T) -> Result<Option<Self>, Error> {
+        let length_format_identifier = LengthFormatIdentifier::from(reader.read_u8()?);
+        
+        let mut max_number_of_block_length: Vec<u8> = vec![0; length_format_identifier.max_number_of_block_length as usize];
+        reader.read_exact(&mut max_number_of_block_length)?;
+
+        Ok(Some(Self {
+            length_format_identifier,
+            max_number_of_block_length
+      }))
+    }
+
+    fn to_writer<T: std::io::Write>(&self, writer: &mut T) -> Result<usize, Error> {
+        writer.write_u8(self.length_format_identifier.into())?;
+        writer.write_all(&self.max_number_of_block_length)?;
+        Ok(1 + self.max_number_of_block_length.len())
+    }
+}
+
+impl SingleValueWireFormat for RequestDownloadResponse {}
