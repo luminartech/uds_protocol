7a0870b3ac094fcf03b2fa131b05bb53996843bd
Kim <kim@squeakytrees.com>
2025-01-16 22:04:57 -0500

    Remove TransferRequestParameter (It was just an example in the spec)

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -19,8 +19,5 @@ pub use security_access_type::SecurityAccessType;
 mod suppressable_positive_response;
 pub(crate) use suppressable_positive_response::SuppressablePositiveResponse;
 
-mod transfer_request_parameter;
-pub use transfer_request_parameter::TransferRequestParameter;
-
 mod format_identifiers;
 pub(crate) use format_identifiers::{DataFormatIdentifier, LengthFormatIdentifier, MemoryFormatIdentifier};
