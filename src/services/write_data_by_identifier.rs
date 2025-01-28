e652a0ab016fea5325980457c021cd627df2a27e
Matt Beisser <matthew.beisser@luminartech.com>
2025-01-28 10:07:43 -0500

    Refactor request

--- src/services/write_data_by_identifier.rs
+++ src/services/write_data_by_identifier.rs
@@ -13,14 +13,14 @@ const WRITE_DID_NEGATIVE_RESPONSE_CODES: [NegativeResponseCode; 5] = [
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 #[non_exhaustive]
 pub struct WriteDataByIdentifierRequest<Identifier, Payload> {
-    pub identifier: Identifier,
+    // pub identifier: Identifier,
     pub payload: Payload,
 }
 
 impl<Identifier: IterableWireFormat, Payload: IterableWireFormat>
     WriteDataByIdentifierRequest<Identifier, Payload>
 {
-    pub(crate) fn new(identifier: Identifier, payload: Payload) -> Self {
+    pub fn new(identifier: Identifier, payload: Payload) -> Self {
         Self {
             identifier,
             payload,
@@ -45,8 +45,18 @@ impl<Identifier: IterableWireFormat, Payload: IterableWireFormat> WireFormat
     /// Note: The first two bytes in the reader must represent the data identifier, immediately followed by the
     /// corresponding payload for that identifier.    
     fn option_from_reader<R: std::io::Read>(reader: &mut R) -> Result<Option<Self>, Error> {
+        // MJB: What to do? Can't get the identifier separate because option_from_reader needs to know
+        // how to interpret the bytes. If we do that though, we lose the identifier.
+
         let identifier = Identifier::option_from_reader(reader)?.unwrap();
         let payload = Payload::option_from_reader(reader)?.unwrap();
+        // Basically want something like this:
+
+        // Force the payload to convert to Identifier?
+        //let id = Payload::from(payload);
+        // Force it to be constructible with new?
+        //let payload = Payload::new(identifier, reader)?;
+
         Ok(Some(Self::new(identifier, payload)))
     }
 
@@ -59,18 +69,13 @@ impl<Identifier: IterableWireFormat, Payload: IterableWireFormat> WireFormat
     /// Note: The first two bytes of the writer will be the data identifier, immediately followed by the corresponding
     /// payload for that identifier.
     fn to_writer<T: std::io::Write>(&self, writer: &mut T) -> Result<usize, Error> {
-        let mut bytes_written = self.identifier.to_writer(writer)?;
-        bytes_written += self.payload.to_writer(writer)?;
+        // It would be nice if we could do this, but we can't
+        //let mut bytes_written = self.identifier.to_writer(writer)?;
+        // let bytes_written = self.payload.to_writer(writer)?;
+        // Ok(bytes_written)
+
+        // Payload must implement the extra bytes, because option_from_reader needs to know how to interpret payload message
+        let bytes_written = self.payload.to_writer(writer)?;
         Ok(bytes_written)
     }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    // use crate::iris_generic_diagnostics;
-
-    fn deserialize_write_data_by_identifier() {}
-
-    fn serialize_write_data_by_identifier() {}
 }
