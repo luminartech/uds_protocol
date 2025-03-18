9bdfeada6e688bdc7c6867e4b18c21aff8d214f8
Kim <kim@squeakytrees.com>
2025-03-18 01:47:27 -0400

    Update Response for routine control

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,8 @@ pub type ProtocolRequest =
     Request<ProtocolIdentifier, ProtocolIdentifier, ProtocolPayload, ProtocolPayload>;
 
 /// Type alias for a UDS Response type that only implements the messages explicitly defined by the UDS specification.
-pub type ProtocolResponse = Response<ProtocolIdentifier, ProtocolPayload>;
+pub type ProtocolResponse =
+    Response<ProtocolIdentifier, ProtocolIdentifier, ProtocolPayload, ProtocolPayload>;
 
 /// What type of routine control to perform for a [RoutineControlRequest].
 #[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize, ValueEnum)]
