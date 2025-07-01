e641e21a37a0603691177b652adc62f1dc11a77f
Parth Patel <parth.pmech@gmail.com>
2025-07-01 23:45:18 +0530

    test: add asserts for size of macros

--- src/common/primitive_generics.rs
+++ src/common/primitive_generics.rs
@@ -93,6 +93,7 @@ mod tests {
 
         let u8_byte = u8::option_from_reader(&mut reader).unwrap().unwrap();
         assert_eq!(u8_byte, 0xFF);
+        assert_eq!(u8_byte.required_size(), 1);
 
         let mut write_buffer = vec![];
         u8_byte.to_writer(&mut write_buffer).unwrap();
@@ -107,6 +108,7 @@ mod tests {
 
         let u16_byte = u16::option_from_reader(&mut reader).unwrap().unwrap();
         assert_eq!(u16_byte, 0xFF01);
+        assert_eq!(u16_byte.required_size(), 2);
 
         let mut write_buffer = vec![];
         u16_byte.to_writer(&mut write_buffer).unwrap();
@@ -121,6 +123,7 @@ mod tests {
 
         let u32_byte = u32::option_from_reader(&mut reader).unwrap().unwrap();
         assert_eq!(u32_byte, 0xFF200201);
+        assert_eq!(u32_byte.required_size(), 4);
 
         let mut write_buffer = vec![];
         u32_byte.to_writer(&mut write_buffer).unwrap();
@@ -135,6 +138,7 @@ mod tests {
 
         let u64_byte = u64::option_from_reader(&mut reader).unwrap().unwrap();
         assert_eq!(u64_byte, 0xFF200201FF200201);
+        assert_eq!(u64_byte.required_size(), 8);
 
         let mut write_buffer = vec![];
         u64_byte.to_writer(&mut write_buffer).unwrap();
@@ -152,6 +156,7 @@ mod tests {
 
         let u128_byte = u128::option_from_reader(&mut reader).unwrap().unwrap();
         assert_eq!(u128_byte, 0xFF200201FF200201FF200201FF200201);
+        assert_eq!(u128_byte.required_size(), 16);
 
         let mut write_buffer = vec![];
         u128_byte.to_writer(&mut write_buffer).unwrap();
